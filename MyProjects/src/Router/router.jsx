1b99532bf8eeb57c57cf556c60c5261dce569874
faizanmuhammad1 <faizanmuhammad6412@gmail.com>
2023-11-18 19:21:34 +0500

    Bug fix

---
+++ MyProjects/src/Router/router.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+function AppRoutes() {
+    return (
+        <Router>
+       </Router>
+  
+    );
+}
+
+export default AppRoutes;
--- MyProjects/src/main.jsx
+++ MyProjects/src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import AppRoutes from './Router/router'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Navbar/>
+     <AppRoutes />
   </React.StrictMode>,
 )
