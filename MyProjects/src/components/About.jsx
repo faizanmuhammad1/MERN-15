5ea6f6c4c8c839683d96c0384e97ad67fdd1717d
Faizan Muhammad <faizanmuhammad6412@gmail.com>
2023-11-27 17:13:28 +0500

    updated

--- MyProjects/src/components/About.jsx
+++ MyProjects/src/components/About.jsx
@@ -4,27 +4,55 @@ import Character from "./Character";
 import { SocialIcon } from "react-social-icons";
 
 const About = () => {
+  const [isHovered, setIsHovered] = React.useState(false);
   return (
-    <section className="p-10">
+    <section className="w-full">
       <div className="flex justify-center">
         <Character />
       </div>
       <div className="text-center p-4">
-        <h2 className="text-6xl font-bold mb-2 ">I am Faizan Muhammad 👨‍💻</h2>
+        <h2 className="text-2xl md:text-4xl lg:text-6xl font-bold mb-2 ">
+          I am Faizan Muhammad 👨‍💻
+        </h2>
       </div>
-      <div className="text-center text-4xl">
+      <div className="text-center text-sm	md:text-xl lg:text-4xl">
         <p>
           A passionate learner currently focusing on MERN Stack development.
           &#x2764;
         </p>
       </div>
-      <div className="flex justify-center gap-6 my-8">
-        <SocialIcon url="https://github.com" />
-        <SocialIcon url="https://x.com" />
-        <SocialIcon url="https://leetcode.com" />
-        <SocialIcon url="https://linkedin.com" />
-        <SocialIcon url="https://telegram.com" />
-        <SocialIcon url="https://discord.com" />
+      <div className="flex justify-center gap-2 md:gap-4 lg:gap-6 my-8">
+        <SocialIcon
+          className="hover:opacity-60"
+          target="_blank"
+          url="https://github.com/faizanmuhammad1"
+        />
+        <SocialIcon
+          className="hover:opacity-60"
+          target="_blank"
+          url="https://twitter.com/faizanmuhammadd"
+        />
+        <SocialIcon
+          className="hover:opacity-60"
+          target="_blank"
+          url="https://leetcode.com/bhattibhai/"
+        />
+        <SocialIcon
+          className="hover:opacity-60"
+          target="_blank"
+          url="https://www.linkedin.com/in/faizanmuhammad2/"
+        />
+        <SocialIcon
+          className="hover:opacity-60"
+          target="_blank"
+          network="telegram"
+          url="https://t.me/faizanmuhammad"
+        />
+        <SocialIcon
+          className="hover:opacity-60"
+          target="_blank"
+          url="https://https://discord.com/users/572489048464293898"
+        />
       </div>
     </section>
   );
