d78455540a29c525a3d95627ae3fa6202aa10e19
faizanmuhammad1 <faizanmuhammad6412@gmail.com>
2023-12-24 14:34:26 +0500

    updated

--- Back-end/app.js
+++ Back-end/app.js
@@ -1,13 +1,10 @@
-
-const express = require('express');
-const cors = require('cors');
-const connectDatabase = require('./database/connection');
-const personRoutes = require('./routes/person.routes');
-const movieRoutes = require('./routes/movies.routes');
-const personRoutes = require('./routes/persons.routes');
-const productRoutes = require('./routes/product.routes');
-
-
+const express = require("express");
+const cors = require("cors");
+const connectDatabase = require("./database/connection");
+const personRoutes = require("./routes/person.routes");
+const movieRoutes = require("./routes/movies.routes");
+const personsRoutes = require("./routes/persons.routes");
+const productRoutes = require("./routes/product.routes");
 const app = express();
 const port = 8000;
 
@@ -16,18 +13,17 @@ app.use(cors());
 
 connectDatabase();
 
-app.use('/persons', personRoutes);
-app.use('/movies', movieRoutes);
-app.use('/persons', personRoutes);
-app.use('/products', productRoutes);
-
+app.use("/persons", personRoutes);
+app.use("/movies", movieRoutes);
+app.use("/persons", personRoutes);
+app.use("/products", productRoutes);
 
 app.listen(port, (err) => {
-    if (err) {
-        console.error('Error starting server:', err);
-    } else {
-        console.clear();
-        console.log(`Server is live on ${port}`);
-    }
+  if (err) {
+    console.error("Error starting server:", err);
+  } else {
+    console.clear();
+    console.log(`Server is live on ${port}`);
+  }
 });
 module.exports = app;
